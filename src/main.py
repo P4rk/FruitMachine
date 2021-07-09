d8aaa5df19e384ba07e1f73073570777cfab90e3
Luke Park <luke@mpb.com>
2021-07-09 14:19:20 +0100

    Add logging test command

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import logging
 import random
 from time import sleep
 
@@ -6,6 +7,8 @@ from discord.ext import commands
 
 import guild_icon_changer
 import properties
+
+logger = logging.getLogger(__name__)
 
 
 description = '''
@@ -23,6 +26,16 @@ async def on_ready():
 @bot.command(pass_context=True)
 async def playing(ctx, game: str):
     await bot.change_presence(activity=discord.Game(name=game))
+
+
+@bot.command(pass_context=True)
+async def test_logging(ctx):
+    logger.debug('debug 10')
+    logger.info('info 20')
+    logger.warning('warning 30')
+    logger.error('error 40')
+    logger.fatal('fatal 50')
+    logger.critical('critical 50')
 
 
 @bot.command(pass_context=True)
@@ -56,6 +69,10 @@ async def roll(ctx, dice: str):
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
 
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+logger.addHandler(ch)
 
 bot.loop.create_task(guild_icon_changer.icon_changer_task(bot))
 bot.run(properties.TOKEN)
